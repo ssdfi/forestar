6f89f3f53db0f2dc175bdd5aee05d838077ab3b5
Martin Dell Oro <martindell.oro@gmail.com>
2018-08-09 09:13:08 -0300

    Cobros de beneficios js

--- web/js/cobros_beneficios.js
+++ web/js/cobros_beneficios.js
@@ -1,31 +1,41 @@
 $(document).ready(function () {
 
-  $('#add-actividad-sig').click(function (e) {
+  $('#add-cobro-beneficio').click(function (e) {
     e.preventDefault();
-    actividadInspeccionadaAdd();
+    cobroBeneficioAdd();
     return;
   });
 
 
-  $(document).on( "click",'.remove-actividad-sig',function (e) {
+  $(document).on( "click",'.remove-cobros-beneficio',function (e) {
     e.preventDefault();
-    actividadInspeccionadaRemove(this);
+    cobroBeneficioRemove(this);
     return;
   });
 
-  function actividadInspeccionadaAdd() {
-      var collectionHolder = $('.actividadesSig');
+  function cobroBeneficioAdd() {
+      var collectionHolder = $('.cobrosBeneficios');
       var collectionCount = collectionHolder.children().length;
       var prototipo = collectionHolder.attr('data-prototype');
+      prototipo = prototipo.replace(/___count___/g, (collectionCount+1));
+      prototipo = prototipo.replace(/___heading___/g, 'heading'+(collectionCount+1));
+      prototipo = prototipo.replace(/___collapse___/g, 'collapse'+(collectionCount+1));
       prototipo = prototipo.replace(/__name__/g, collectionCount);
       collectionCount++;
-      var newLi = jQuery('<tr class="fields"></tr>').html(prototipo);
+      var newLi = jQuery('<div class="card"></tr>').html(prototipo);
       newLi.appendTo(collectionHolder).trigger('create');
+      var nombre = 'appbundle_expedientes_cobroBeneficios_'+(collectionCount-1)+'_nombreBanco';
+      var nombreBanco = document.getElementById(nombre);
+      var headName = ".head-name"+(collectionCount);
+      $(nombreBanco).keyup(function() {
+          $(headName).text(this.value);
+      });
       return;
   }
 
-  function actividadInspeccionadaRemove(btn) {
-    var row = $(btn).closest('.fields');
+  function cobroBeneficioRemove(btn) {
+    var row = $(btn).closest('.card');
+    console.log(row);
     row.remove();
     return false;
   }
