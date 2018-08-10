4ed4f3ffe6015486ffcaec2f4675f627766666bb
Martin Dell Oro <martindell.oro@gmail.com>
2018-08-10 17:26:53 -0300

    Adding Actividades Aprobadas

--- src/AppBundle/Form/ActividadesAprobadasType.php
+++ src/AppBundle/Form/ActividadesAprobadasType.php
@@ -5,6 +5,10 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ActividadesAprobadasType extends AbstractType
 {
@@ -13,15 +17,15 @@ class ActividadesAprobadasType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-          ->add('especie')
-          ->add('superficieHa')
-          ->add('densidad')
-          ->add('fechaInicio')
-          ->add('fechaFinActividad')
-          ->add('departamento')
-          ->add('tipoActividad')
-          ->add('observaciones');
+      $builder
+                ->add('especie', EntityType::class, array('class'=>'AppBundle\Entity\Especies','required'=>true,'label' => false))
+                ->add('superficieHa', IntegerType::class, array('label'=>false,'required'=>false))
+                ->add('densidad', IntegerType::class, array('label'=>false,'required'=>false))
+                ->add('fechaInicio', DateType::class, array('label' => false,'widget'=>'single_text','format' => 'yyyy-MM-dd','required'=>false,'attr' => array('class' => 'form-control','placeholder'=>"AAAA-MM-DD")))
+                ->add('fechaFinActividad', DateType::class, array('label' => false,'widget'=>'single_text','format' => 'yyyy-MM-dd','required'=>false,'attr' => array('class' => 'form-control','placeholder'=>"AAAA-MM-DD")))
+                ->add('departamento',EntityType::class, array('class'=>'AppBundle\Entity\Departamentos','required'=>true,'label' => false))
+                ->add('tipoActividad',EntityType::class, array('class'=>'AppBundle\Entity\TiposActividades','required'=>true,'label' => false))
+                ->add('observaciones',TextareaType::class,array('label' => false,'required'=>false,'attr' => array('class' => 'form-control')));
     }/**
      * {@inheritdoc}
      */
